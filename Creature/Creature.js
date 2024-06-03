461e36d44c48a0346d1ceff7fdb6213a74d13dcf
Sabshane <91722022+Sabshane@users.noreply.github.com>
2024-06-03 09:59:14 +0200

    Clean import

---
+++ Creature/Creature.js
@@ -0,0 +1,4 @@
+export {default  as searchCreature} from "./searchCreature.js";
+export { default as getCreature}  from "./getCreature.js";
+export { default as getCreatureIndex}  from "./getCreatureIndex.js";
+
